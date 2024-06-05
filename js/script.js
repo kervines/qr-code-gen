1e30f56bc1ed7f8f73eab0349a278ef8a732e809
kervines <kervinzero@gmail.com>
2024-06-05 09:44:40 -0300

    fix - bug que gera varias mensagens de alerta

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 const qrcodeEl = document.getElementById('qrcode');
 const formEl = document.querySelector('form');
 const inputURL = document.getElementById('url');
+const btnSubmit = document.querySelector('input[type="submit"]');
 
 let stateQRCode = true;
 formEl.addEventListener('submit', (e) => {
   e.preventDefault();
-
   const url = `https://${inputURL.value}`;
 
   if (!inputURL.value) {
@@ -13,8 +13,10 @@ formEl.addEventListener('submit', (e) => {
     p.innerText = 'Digite sua URL';
     p.style.color = '#d52742';
     formEl.appendChild(p);
+    btnSubmit.style.pointerEvents = 'none';
     setTimeout(() => {
       formEl.removeChild(p);
+      btnSubmit.style.pointerEvents = 'visible';
     }, 2000);
   } else {
     qrcodeEl.innerHTML = '';
